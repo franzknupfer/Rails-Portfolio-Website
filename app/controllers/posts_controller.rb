e18cc390d0c812430767bcd656e504306b185e26
Franz <franzknupfer@hotmail.com>
2015-07-10 10:37:56 -0700

    build out posts controller, adding paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,24 +15,37 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      flash[:notice] = "This post has been saved."
+      flash[:notice] = "Post saved."
+      redirect_to posts_path
     else
       flash[:notice] = "Please try again."
+      render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post updated."
+      redirect_to posts_path
+    else
+      flash[:notice] = "Please try again."
+      render :edit
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:post_title, :post_content)
+    params.require(:post).permit(:post_title, :post_content, :attached_image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   validates :post_title, :presence => true
   validates :post_content, :presence => true
+
+  has_attached_file :attached_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :attached_image, :content_type => /\Aimage\/.*\Z/
 end
