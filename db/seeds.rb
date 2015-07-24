f34f0ec3f3b78c48e1cc30d996bc03728ea7ce41
Franz <franzknupfer@hotmail.com>
2015-07-23 23:37:55 -0700

    seed admin and adjust production.rb to hopefully get background image working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+users = User.create([email:"franzknupfer@hotmail.com", username: "franz", password: "testtest", password_confirmation: "testtest", admin: true])
